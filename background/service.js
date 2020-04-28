433019ead67638d039b1afd3317b3f4f282ed38f
Gomita <gomita@xuldev.org>
2020-04-28 11:44:05 +0900

    enable multiple tabs selection: part 2
    reimplement context menu for multiple tabs

--- background/service.js
+++ background/service.js
@@ -100,48 +100,89 @@ async function handleMenuShown(info, tab) {
 
 async function handleMenuClick(info, tab) {
 //	console.log(JSON.stringify(info) + "\n\n"+ JSON.stringify(tab));
+	// decide the context tabs which the command will be applied to.
+	// note that if the clicked tab is not highlighted, the context tab is it only.
+	let tabs = !tab.highlighted ? [tab] : 
+	           await browser.tabs.query({ currentWindow: true, highlighted: true });
 	switch (info.menuItemId) {
-		case "reload"       : browser.tabs.reload(tab.id); break;
-		case "mute"         : browser.tabs.update(tab.id, { muted: true }); break;
-		case "unmute"       : browser.tabs.update(tab.id, { muted: false }); break;
-		case "pin"          : browser.tabs.update(tab.id, { pinned: true }); break;
-		case "unpin"        : browser.tabs.update(tab.id, { pinned: false }); break;
-		case "duplicate"    : browser.tabs.duplicate(tab.id); break;
-		case "moveToTop"    : 
-			if (tab.pinned) {
-				browser.tabs.move(tab.id, { index: 0 });
+		case "reload"       : tabs.map(_tab => browser.tabs.reload(_tab.id)); break;
+		case "mute"         : tabs.map(_tab => browser.tabs.update(_tab.id, { muted: true })); break;
+		case "unmute"       : tabs.map(_tab => browser.tabs.update(_tab.id, { muted: false })); break;
+		case "pin"          : tabs.map(_tab => browser.tabs.update(_tab.id, { pinned: true })); break;
+		case "unpin"        : tabs.reverse().map(_tab => browser.tabs.update(_tab.id, { pinned: false })); break;
+		case "duplicate"    : 
+			let dupTabIds = [];
+			for (let _tab of tabs) {
+				let dupTab = await browser.tabs.duplicate(_tab.id);
+				dupTabIds.push(dupTab.id);
 			}
-			else {
-				let pins = await browser.tabs.query({ currentWindow: true, pinned: true });
-				browser.tabs.move(tab.id, { index: pins.length });
+			// move duplicated tabs to the next of last context tab
+			let prefs = await browser.storage.local.get();
+			if (!prefs.stacking)
+				browser.tabs.move(dupTabIds, { index: tabs[tabs.length - 1].index + 1 });
+			break;
+		case "moveToTop"    : 
+			tabs = tabs.reverse();
+			for (let _tab of tabs) {
+				if (_tab.pinned) {
+					browser.tabs.move(_tab.id, { index: 0 });
+				}
+				else {
+					let pins = await browser.tabs.query({ currentWindow: true, pinned: true });
+					browser.tabs.move(_tab.id, { index: pins.length });
+				}
 			}
 			break;
 		case "moveToBottom" : 
-			if (tab.pinned) {
-				let pins = await browser.tabs.query({ currentWindow: true, pinned: true });
-				browser.tabs.move(tab.id, { index: pins.length - 1});
-			}
-			else {
-				browser.tabs.move(tab.id, { index: -1 });
+			for (let _tab of tabs) {
+				if (_tab.pinned) {
+					let pins = await browser.tabs.query({ currentWindow: true, pinned: true });
+					browser.tabs.move(_tab.id, { index: pins.length - 1 });
+				}
+				else {
+					browser.tabs.move(_tab.id, { index: -1 });
+				}
 			}
 			break;
-		case "detach"       : browser.windows.create({ tabId: tab.id, incognito: tab.incognito }); break;
+		case "detach"       : 
+			let _tab = tabs.shift();
+			let win = await browser.windows.create({ tabId: _tab.id, incognito: _tab.incognito });
+			let tabIds = tabs.map(_tab => _tab.id);
+			browser.tabs.move(tabIds, { windowId: win.id, index: -1 });
+			break;
 		case "closeToTop"   : 
 		case "closeToBottom": 
 		case "closeOther"   : 
-			let ref = await browser.tabs.get(tab.id);
-			let tabs = await browser.tabs.query({ currentWindow: true });
-			tabs = tabs.filter(tab => !tab.hidden && !tab.pinned);
-			if (info.menuItemId == "closeToBottom")
-				tabs = tabs.filter(tab => tab.index > ref.index);
-			else if (info.menuItemId == "closeToTop")
-				tabs = tabs.filter(tab => tab.index < ref.index);
-			else if (info.menuItemId == "closeOther")
-				tabs = tabs.filter(tab => tab.id != ref.id);
-//			if (tabs.length > 1 && 
-//			    !window.confirm(browser.i18n.getMessage("closeConfirm", [tabs.length])))
+			let remTabs = await browser.tabs.query({ currentWindow: true });
+			remTabs = remTabs.filter(_tab => !_tab.hidden && !_tab.pinned);
+			if (info.menuItemId == "closeToBottom") {
+				if (!tab.highlighted)
+					// 0 [1] 2 [3] 4 [5] 6 --> context on 2 --> removing 3,4,5,6
+					remTabs = remTabs.filter(_tab => _tab.index > tab.index);
+				else
+					// 0 [1] 2 [3] 4 [5] 6 --> context on 3 --> removing 4,6
+					remTabs = remTabs.filter(_tab => _tab.index > tab.index && !_tab.highlighted);
+			}
+			else if (info.menuItemId == "closeToTop") {
+				if (!tab.highlighted)
+					// 0 [1] 2 [3] 4 [5] 6 --> context on 4 --> removing 0,1,2,3
+					remTabs = remTabs.filter(_tab => _tab.index < tab.index);
+				else
+					// 0 [1] 2 [3] 4 [5] 6 --> context on 5 --> removing 0,2,4
+					remTabs = remTabs.filter(_tab => _tab.index < tab.index && !_tab.highlighted);
+			}
+			else if (info.menuItemId == "closeOther") {
+				if (!tab.highlighted)
+					// 0 [1] 2 [3] 4 [5] 6 --> context on 2 --> removing 0,1,3,4,5,6
+					remTabs = remTabs.filter(_tab => _tab.index != tab.index);
+				else
+					// 0 [1] 2 [3] 4 [5] 6 --> context on 3 --> removing 0,2,4,6
+					remTabs = remTabs.filter(_tab => _tab.index != tab.index && !_tab.highlighted);
+			}
+//			if (remTabs.length > 1 && 
+//			    !window.confirm(browser.i18n.getMessage("closeConfirm", [remTabs.length])))
 //				return;
-			tabs.map(_tab => browser.tabs.remove(_tab.id));
+			remTabs.map(_tab => browser.tabs.remove(_tab.id));
 			break;
 		case "undoClose": 
 			let sessionInfos = await browser.sessions.getRecentlyClosed({ maxResults: 1 });
@@ -149,14 +190,14 @@ async function handleMenuClick(info, tab) {
 				return;
 			browser.sessions.restore(sessionInfos[0].tab.sessionId);
 			break;
-		case "close": browser.tabs.remove(tab.id); break;
+		case "close": tabs.map(_tab => browser.tabs.remove(_tab.id)); break;
 		default: 
 			if (!/^reopen:(.+)$/.test(info.menuItemId))
 				break;
-			browser.tabs.create({
-				url: tab.url, index: ++tab.index, pinned: tab.pinned, active: true, 
+			tabs.reverse().map(_tab => browser.tabs.create({
+				url: _tab.url, index: ++_tab.index, pinned: _tab.pinned, active: true, 
 				cookieStoreId: RegExp.$1 == "default" ? undefined : RegExp.$1
-			});
+			}));
 			break;
 	}
 }
